861fab054ee1ad1d334018b94302a45d6a9a970e
Marshall Bruner <brunerm99@gmail.com>
2020-05-28 07:39:41 -0600

    initial commit

---
+++ KeepEdit.py
@@ -0,0 +1,27 @@
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+#username: brunerm99
+#password: fczqbrnkjmzvptut
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+    keepObj = createKeepObject()
+    print(loginNow(keepObj, 'brunerm99', 'fczqbrnkjmzvptut'))
+    addNote(keepObj, 'title', 'info')
+    update(keepObj)
+
+***REMOVED***
+***REMOVED***
